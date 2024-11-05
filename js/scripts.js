c916cc379381a41937db9af6001286a353b1254f
tristan-2000-00 <tristan2000@gmail.com>
2024-11-05 09:26:34 -0700

    Updated script file code and moved console.log to the top

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+console.log("Welcome to my portfolio!")
+
 function menuToggle() {
   var nav = document.getElementById('main-nav');
   if (nav.className === 'navtoggle') {
@@ -5,5 +7,4 @@ function menuToggle() {
   } else {
     nav.className = 'navtoggle';
   }
-  // console.log("Welcome to my portfolio!");
 }
