522260e2e61ff0e78265b8a5897df7a161485c66
tristan-2000-00 <tristan2000@gmail.com>
2024-11-04 18:28:22 -0700

    Added hambuger menu for screens below 641px

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Welcome to my portfolio!");
+function menuToggle() {
+  var nav = document.getElementById('main-nav');
+  if (nav.className === 'navtoggle') {
+    nav.className += ' show';
+  } else {
+    nav.className = 'navtoggle';
+  }
+  // console.log("Welcome to my portfolio!");
+}
